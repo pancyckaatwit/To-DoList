f7cd9ccb0170fb0bbc070dd7a15a70f39f7dc3ed
pancyckaatwit <54997253+pancyckaatwit@users.noreply.github.com>
2022-12-22 20:06:55 -0500

    Created a rough layout of the classes.

---
+++ title.java
@@ -0,0 +1,4 @@
+// WIll handle the title bar section of the todo list
+public class title {
+    
+}
---
+++ todo.java
@@ -0,0 +1,7 @@
+// Main class for the program
+public class todo {
+    
+    public static void main(String args[]) {
+        
+    }
+}
