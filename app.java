657028a898db671c72a71983a8e9c770b59e2500
pancyckaatwit <54997253+pancyckaatwit@users.noreply.github.com>
2022-12-22 20:47:04 -0500

    Called app class in todo and set variables in app.

--- app.java
+++ app.java
@@ -1,4 +1,15 @@
+import javax.swing.JFrame;
+
 // Will be the JFrame for the project
-public class app {
-    
+public class app extends JFrame {
+    // Variables
+    private title titlePanel;
+        private list l;
+    private button buttonPanel;
+
+
+    //Constructor for app
+    app() {
+        
+    }
 }
--- todo.java
+++ todo.java
@@ -1,7 +1,7 @@
-// Main class for the program
+// Main class for the program that calls the app class
 public class todo {
     
     public static void main(String args[]) {
-        
+        new app();
     }
 }
